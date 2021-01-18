9f2c29de78777182cdd5dfe6df236995d930f5b0
Sam Rogers <samrogers15@gmail.com>
2021-01-18 11:50:50 -0800

    updating changes to source code

--- script.js
+++ script.js
@@ -7,45 +7,58 @@ function getWeatherInfo(city) {
         url: queryURL,
         method: "GET"
     })
+    .then(function(response){
+        console.log(response);
+
+        var currentDate = moment().format('M/D/YYYY');
+        $('#current-city-name').html('<h1>' + response.name + ' Weather Details for ' + currentDate +'</h1>');
+        var forecastPic = response.weather[0].icon;
+        $('#weather-img').attr('src', 'https://openweathermap.org/img/w/' + forecastPic + '.png');
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $('#temp').text('Temperature: ' + tempF.toFixed(0) + '\u00B0 Fahrenheit');
+        $('#humidity').text('Humidity: ' + response.main.humidity + '%');
+        $('#speed').text('Wind Speed ' + response.wind.speed + ' mph');
+        
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+
+        var UVIndexQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + APIKey;
+
+        $.ajax({
+            url: UVIndexQueryURL,
+            method: "GET"
+        })
         .then(function(response){
             console.log(response);
 
-            var currentDate = moment().format('M/D/YYYY');
-            $('#current-city-name').html('<h1>' + response.name + ' Weather Details for ' + currentDate +'</h1>');
-            var forecastPic = response.weather[0].icon;
-            $('#weather-img').attr('src', 'https://openweathermap.org/img/w/' + forecastPic + '.png');
-            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-            $('#temp').text('Temperature: ' + tempF.toFixed(0) + '\u00B0 Fahrenheit');
-            $('#humidity').text('Humidity: ' + response.main.humidity + '%');
-            $('#speed').text('Wind Speed ' + response.wind.speed + ' mph');
-            
-            var lat = response.coord.lat;
-            var lon = response.coord.lon;
+            var UVI = response.current.uvi;
+            $('#uv-index').text('UV Index: ' + UVI);
 
-            var UVIndexQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + APIKey;
+            for (let i = 1; i < 6; i++) {
+                const card = $('<div>').attr('class', 'card');
+                const cardBody = $('<div>').attr('class', 'card-body');
+                
+                $('#future-weather2').append(card);
 
-            $.ajax({
-                url: UVIndexQueryURL,
-                method: "GET"
-            })
-                .then(function(response){
-                    console.log(response);
+                card.append(cardBody);
+                
+                var date = $('<span>').attr('class', 'card-title').html(moment().add(i, 'days').format('M/D/YYYY') + '<br>');
+                cardBody.append(date);
+                
+                var dailyForecastPic = $('<img>').attr('src', 'https://openweathermap.org/img/w/' + response.daily[i].weather[0].icon + '.png');
+                cardBody.append(dailyForecastPic)
 
-                    var UVI = response.current.uvi;
-                    $('#uv-index').text('UV Index: ' + UVI);
+                var dailyMaxTemp = $('<div>').attr('class', 'card-text').html('High: ' + ((response.daily[i].temp.max - 273.15) * 1.80 + 32).toFixed(0) + '\u00B0 F');
+                cardBody.append(dailyMaxTemp);
 
-                    for (let i = 0; i < 6; i++) {
-                        var date = moment().add(i+1, 'days').format('M/D/YYYY');
-                        var dailyForecastPic = response.daily[i].weather[0].icon;
-                        var dailyMaxTemp = ((response.daily[i].temp.max - 273.15) * 1.80 + 32).toFixed(0);
-                        var dailyMinTemp = ((response.daily[i].temp.min - 273.15) * 1.80 + 32).toFixed(0);
-                        var dailyHumidity = response.daily[i].humidity;
-                    };
-            });
+                var dailyMinTemp = $('<div>').attr('class', 'card-text').html('Low: ' + ((response.daily[i].temp.min - 273.15) * 1.80 + 32).toFixed(0) + '\u00B0 F');
+                cardBody.append(dailyMinTemp);
+
+                var dailyHumidity = $('<div>').attr('class', 'card-text').html('Humidity: ' + response.daily[i].humidity);
+                cardBody.append(dailyHumidity);
+            };
         });
-    
-    
-
+    });
 };
 
 
