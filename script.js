0f55ba7bb384d33715312a02b03fc6b2b2888270
Sam Rogers <samrogers15@gmail.com>
2021-01-18 19:36:24 -0800

    committing updates to js code

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function getWeatherInfo(city) {
+
+    $('#forecast-container').hide();
+
     var APIKey = 'fd8f466eccd599a5426718b131296a81';
-    
+
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
     
     $.ajax({
@@ -10,11 +13,18 @@ function getWeatherInfo(city) {
     .then(function(response){
         console.log(response);
 
-        var currentDate = moment().format('M/D/YYYY');
-        $('#current-city-name').html('<h1>' + response.name + ' Weather Details for ' + currentDate +'</h1>');
+        var currentDate = moment().format('M/D/YYYY'); 
         var forecastPic = response.weather[0].icon;
-        $('#weather-img').attr('src', 'https://openweathermap.org/img/w/' + forecastPic + '.png');
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+        $('#current-city-name').empty();
+        $('#weather-img').empty();
+        $('#temp').empty();
+        $('#humidity').empty();
+        $('#speed').empty();
+
+        $('#current-city-name').html('<h1>' + response.name + ' Weather Details for ' + currentDate +'</h1>');
+        $('#weather-img').attr('src', 'https://openweathermap.org/img/w/' + forecastPic + '.png');
         $('#temp').text('Temperature: ' + tempF.toFixed(0) + '\u00B0 Fahrenheit');
         $('#humidity').text('Humidity: ' + response.main.humidity + '%');
         $('#speed').text('Wind Speed ' + response.wind.speed + ' mph');
@@ -37,30 +47,35 @@ function getWeatherInfo(city) {
             for (let i = 1; i < 6; i++) {
                 const card = $('<div>').attr('class', 'card');
                 const cardBody = $('<div>').attr('class', 'card-body');
-                
+                const date = $('<span>').attr('class', 'card-title').html(moment().add(i, 'days').format('M/D/YYYY') + '<br>');
+                const dailyForecastPic = $('<img>').attr('src', 'https://openweathermap.org/img/w/' + response.daily[i].weather[0].icon + '.png');
+                const dailyMaxTemp = $('<div>').attr('class', 'card-text').html('High: ' + ((response.daily[i].temp.max - 273.15) * 1.80 + 32).toFixed(0) + '\u00B0 F');
+                const dailyMinTemp = $('<div>').attr('class', 'card-text').html('Low: ' + ((response.daily[i].temp.min - 273.15) * 1.80 + 32).toFixed(0) + '\u00B0 F');
+                const dailyHumidity = $('<div>').attr('class', 'card-text').html('Humidity: ' + response.daily[i].humidity);
+
+                cardBody.empty();
+
                 $('#future-weather2').append(card);
-
                 card.append(cardBody);
-                
-                var date = $('<span>').attr('class', 'card-title').html(moment().add(i, 'days').format('M/D/YYYY') + '<br>');
                 cardBody.append(date);
-                
-                var dailyForecastPic = $('<img>').attr('src', 'https://openweathermap.org/img/w/' + response.daily[i].weather[0].icon + '.png');
                 cardBody.append(dailyForecastPic)
-
-                var dailyMaxTemp = $('<div>').attr('class', 'card-text').html('High: ' + ((response.daily[i].temp.max - 273.15) * 1.80 + 32).toFixed(0) + '\u00B0 F');
                 cardBody.append(dailyMaxTemp);
-
-                var dailyMinTemp = $('<div>').attr('class', 'card-text').html('Low: ' + ((response.daily[i].temp.min - 273.15) * 1.80 + 32).toFixed(0) + '\u00B0 F');
                 cardBody.append(dailyMinTemp);
-
-                var dailyHumidity = $('<div>').attr('class', 'card-text').html('Humidity: ' + response.daily[i].humidity);
                 cardBody.append(dailyHumidity);
             };
         });
     });
 };
 
+const searchButton = $('#search');
 
+searchButton.on('click', function(event){
+    event.preventDefault();
+    const citySearch = $('#search-input').val();
+    getWeatherInfo(citySearch);
+    $('#forecast-container').show();
+});
 
-getWeatherInfo('Portland');
+// why does it not work as running the getWeatherInfo as the click function? as in:
+
+// searchButton.on('click', getWeatherInfo())
